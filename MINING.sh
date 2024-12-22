0462ccd937610fecf9158f4060e236877fbd4b12
arshaq0142 <jessiearshaq@gmail.com>
2024-12-22 17:01:30 +0700

    Create MINING.sh

---
+++ MINING.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz &&./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433-U -u
